931eb52131cba1d4dddd6f0bc5f12fda98aad3b0
tntmancer <tntmancer@gmail.com>
2024-05-06 16:47:16 -0400

    a3 css sp22

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First Example (Netify branches)!
         </p>
         <a
           className="App-link"
